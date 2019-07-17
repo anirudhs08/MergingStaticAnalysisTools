a35e2448ecbba813e92daa1cd11be4d31b91d889
anirudh.satyam <anirudhs08@gmail.com>
2019-07-17 16:38:37 +0530

    Batch file done

---
+++ AutoBuildTest.bat
@@ -0,0 +1,3 @@
+MSBuild.exe C:\Users\320053936\source\repos\CaseStudy1\CaseStudy1.sln /property:Configuration=Debug
+"C:\Users\320053936\source\repos\CaseStudy1\CaseStudy1\bin\Debug\CaseStudy1.exe"
+pause
---
+++ CaseStudy1/CaseStudy1/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaseStudy1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Teacher d = new Teacher();
+            d.Teach();
+            Student s = new Student();
+            s.Learn();
+            s.Teach();
+            Console.ReadKey();
+        }
+        class Teacher
+        {
+            public void Teach()
+            {
+                Console.WriteLine("Teach");
+            }
+        }
+        class Student : Teacher
+        {
+            public void Learn()
+            {
+                Console.WriteLine("Learn");
+
+            }
+        }
+    }
+}
